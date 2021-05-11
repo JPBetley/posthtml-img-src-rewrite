00bc84f0c6cfcd2d2124e1d771f7c3be6ee23b5e
Phil Betley <jpbetley@gmail.com>
2021-05-11 15:35:41 -0400

    feat(base): add prefix and suffix functionality

--- test/test-core.js
+++ test/test-core.js
@@ -7,14 +7,33 @@ const path = require('path');
 const posthtml = require('posthtml');
 const fixtures = path.join(__dirname, 'fixtures');
 
-test('basic', t => {
-  return compare(t, 'basic');
+test('options default to empty string', t => {
+  return compare(t, 'default');
 });
 
-async function compare(t, name) {
+test('string options', t => {
+  return compare(t, 'basic', {prefix: 'prefix_', suffix: '_suffix'});
+});
+
+test('function options', t => {
+  return compare(t, 'basic', {
+    prefix() {
+      return 'prefix_';
+    },
+    suffix() {
+      return '_suffix';
+    }
+  });
+});
+
+test('no src', t => {
+  return compare(t, 'no-src');
+});
+
+async function compare(t, name, options) {
   const source = readFileSync(path.join(fixtures, `${name}.html`), 'utf8');
   const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8');
-  const {html} = await posthtml([plugin()]).process(source);
+  const {html} = await posthtml([plugin(options)]).process(source);
 
   t.deepEqual(html, expected);
 }
